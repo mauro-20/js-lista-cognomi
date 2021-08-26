329f856e870ace6ed4df1f4de2c42b970dac1f84
Mauro Tomasoni <mauro.tom.vn@gmail.com>
2021-08-26 14:49:21 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// 1. chiedi all’utente il cognome;
+// 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+// 3. stampa la lista ordinata alfabeticamente;
+// 4. scrivi anche la posizione "umana"(partendo da 1) della lista in cui il nuovo utente si trova;
+
+var lastName = ['bianchi', 'neri', 'rossi', 'verdi', 'gialli']
+
+var userLastName = prompt('Inserisci il tuo cognome').toLowerCase();
+lastName.push(userLastName)
+
+lastName.sort()
+console.log(lastName);
+
+console.log(`La posizione di ${userLastName} nella lista di cognomi è ${lastName.indexOf(userLastName)+1}`);
+
